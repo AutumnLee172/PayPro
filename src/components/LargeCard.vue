a7f476810c992a1a0002a14b92f8fc1fef3992b9
AutumnLee172 <78860600+AutumnLee172@users.noreply.github.com>
2023-04-14 14:00:46 +0800

    login page design

---
+++ src/components/LargeCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title class="font-weight-bolder px-2 mt-2 title" :style="{ color: textColor }">{{ Title
+                }}</ion-card-title>
+                <ion-card-subtitle class="px-2">{{ Subtitle }}</ion-card-subtitle>
+            </ion-card-header>
+
+            <ion-card-content>
+                <slot></slot>
+            </ion-card-content>
+        </ion-card>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+defineProps({
+    Title: String,
+    Subtitle: String,
+    textColor: {
+        type: String,
+        default: "white",
+    },
+});
+</script>
+
+<style scoped>
+
+.title{
+    font-family: "Open Sans", sans-serif;
+}
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,60 @@
 <template>
-    <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Login</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">Login</ion-title>
-          </ion-toolbar>
-        </ion-header>
-  
-        <ExploreContainer name="Login" />
-        <ion-button color="primary" @click="login()">Login</ion-button>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
-  import ExploreContainer from '@/components/ExploreContainer.vue';
-  import { useRouter } from 'vue-router';
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-grid class="card-center">
+        <ion-col class="px-2">
+          <LargeCard Title="Sign In" Subtitle="Enter your email and password" class="ion-justify-content-start">
 
-  const router = useRouter();
+            <ion-row class="my-1">
+            <ion-col>
+              <ion-input class="form-control textfield" placeholder="Email" style="color: white;"></ion-input>
+            </ion-col>
+          </ion-row>
 
-  function login(){
-    router.push("/tabs");
-  }
-  </script>
-  
+          <ion-row>
+            <ion-col class="my-1">
+              <ion-input class="form-control textfield" type="password" placeholder="Password" style="color: white;"></ion-input>
+            </ion-col>
+          </ion-row>
+
+            <ion-row>
+              <ion-col class="ion-text-center">
+                <ion-button class="w-100 primary-btn mt-3" @click="login()">Sign In</ion-button>
+              </ion-col>
+            </ion-row>
+
+            <ion-row class="mt-1">
+              <ion-col class="ion-text-center">
+               <ion-label>Not a member? </ion-label> <a href="/tabs" class="hyperlink"> Sign up now </a>
+              </ion-col>
+            </ion-row>
+          </LargeCard>
+        </ion-col>
+      </ion-grid>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonContent, IonCol, IonButton, IonGrid, IonRow, IonInput, IonLabel } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+import LargeCard from '@/components/LargeCard.vue';
+
+const router = useRouter();
+
+function login() {
+  router.push("/tabs");
+}
+</script>
+
+<style scoped>
+.card-center {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 600px;
+}
+
+
+</style>
