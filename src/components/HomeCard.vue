575a7011795b336f8445909c0a0e0eb46ea60937
AutumnLee172 <78860600+AutumnLee172@users.noreply.github.com>
2023-05-21 18:22:42 +0800

    home page first edition

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,34 @@
+<template>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-subtitle class="px-2">{{ Subtitle }}</ion-card-subtitle>
+                <ion-card-title class="font-weight-bolder px-2 mt-2 title">{{ Title
+                }}</ion-card-title>
+            </ion-card-header>
+
+            <ion-card-content>
+                <slot></slot>
+            </ion-card-content>
+        </ion-card>
+</template>
+  
+<script setup lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+defineProps({
+    Title: String,
+    Subtitle: String,
+});
+</script>
+
+<style scoped>
+
+.title{
+    font-family: "Open Sans", sans-serif;
+}
+
+ion-card {
+    --background: linear-gradient(135deg, #4A148C, #673AB7);
+    --color: #F5F5F5;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,195 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Home Page</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
-      <ExploreContainer name="Home Page" />
-      <TestValidation></TestValidation>
-      <ion-label>{{ username }}</ion-label>
+      <HomeCard Subtitle="Total Wallet Value: " Title="RM7000000">
+        <div class="px-2 subtitle">among 5 wallets</div>
+      </HomeCard>
+      <div class="mx-2 row">
+        <div class="col">
+          <ion-button fill="outline">
+            <ion-row class="ion-justify-content-center">
+              <ion-col class="ion-text-center">
+                <ion-icon :icon="star"></ion-icon>
+                <br/>
+                <ion-label>Transfer</ion-label>
+              </ion-col>
+            </ion-row>
+          </ion-button>
+        </div>
+        <div class="col">
+          <ion-button> Test </ion-button>
+        </div>
+        <div class="col">
+          <ion-button> Test </ion-button>
+        </div>
+        <div class="col">
+          <ion-button> Test </ion-button>
+        </div>
+      </div>
+
+      <div class="button-row">
+        <div v-for="(button, index) in buttons" :key="index" class="button-wrapper">
+          <ion-button color="primary" expand="full">
+            <ion-icon :icon="button.icon"></ion-icon>
+            <span>{{ button.label }}</span>
+          </ion-button>
+        </div>
+      </div>
+      <LargeCard Title="Stats">
+        <apexchart type="line" :options="chartOptions" :series="series"></apexchart>
+      </LargeCard>
+      <LargeCard Title="Recent Transactions">
+        <ion-list>
+          <div v-for="(transaction, index) in transactions" :key="index">
+            <ion-item>
+              <ion-label>
+                <div class="row main-text">
+                  <div class="col-8">
+                    <ion-label v-if="transaction.transaction_type == 'Internal' && transaction.to_account == id">Receive
+                      from {{ transaction.wallet_type }}</ion-label>
+                    <ion-label
+                      v-else-if="transaction.transaction_type == 'External' && transaction.to_account == id && transaction.to_wallet_type == 'Linked PayPro Wallet'">Receive
+                      from {{ transaction.wallet_type }}</ion-label>
+                    <ion-label v-else>Transfer to {{ transaction.to_wallet_type }}</ion-label>
+                  </div>
+                  <div class="text-end col">
+                    <ion-label style="color:#7168eeea"
+                      v-if="transaction.transaction_type == 'Internal' && transaction.to_account == id">+ RM {{
+                        transaction.amount }}</ion-label>
+                    <ion-label style="color:#7168eeea"
+                      v-else-if="transaction.transaction_type == 'External' && transaction.to_account == id && transaction.to_wallet_type == 'Linked PayPro Wallet'">+
+                      RM {{ transaction.amount }}</ion-label>
+                    <ion-label v-else> - RM {{ transaction.amount }}</ion-label>
+                  </div>
+                </div>
+                <div class="row my-1">
+                  <div class="col">
+                    <p> {{ transaction.transaction_type }} Transaction </p>
+                  </div>
+                  <div class="text-end col">
+                    <p>{{ transaction.date }}</p>
+                  </div>
+                </div>
+              </ion-label>
+            </ion-item>
+          </div>
+        </ion-list>
+      </LargeCard>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel } from '@ionic/vue';
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonList, IonIcon, IonRow, IonCol } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
-import IonRippleEffect from '@/components/IonRippleEffect.vue';
-import TestValidation from '@/components/TestValidation.vue';
+import HomeCard from '@/components/HomeCard.vue';
+import LargeCard from '@/components/LargeCard.vue';
+import { defineComponent, inject, reactive } from 'vue';
+import VueApexCharts from "vue3-apexcharts";
+import { star, addOutline, walletOutline, settingsOutline } from 'ionicons/icons';
 
-import { inject } from 'vue'
-const apiUrl = inject<string>('API_URL')
-const username = localStorage.getItem('username')
+
+export default defineComponent({
+  props: {
+    name: {
+      default: null
+    },
+    id: {
+      default: null
+    }
+  },
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonLabel,
+    ExploreContainer,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+    HomeCard,
+    LargeCard,
+    IonButton,
+    IonList,
+    IonIcon,
+    IonRow, 
+    IonCol,
+    apexchart: VueApexCharts,
+  },
+  setup() {
+    return { star, addOutline, walletOutline, settingsOutline };
+  },
+  created() {
+
+  },
+  data() {
+    return {
+      apiUrl: inject<string>('API_URL'),
+      form: reactive({
+        userid: localStorage.getItem('userid'),
+      }),
+      transactions: [{ wallet_type: '', amount: 0, to_wallet_type: '', date: '', transaction_type: '', to_account: 0 }],
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+          toolbar: {
+            show: false, // Hide the toolbar
+          },
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996],
+        },
+        stroke: {
+          curve: 'smooth',
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 35, 50, 49, 60],
+        },
+      ],
+      buttons: [
+        { icon: addOutline, label: 'Add' },
+        { icon: walletOutline, label: 'Wallet' },
+        { icon: settingsOutline, label: 'Settings' },
+        { icon: addOutline, label: 'Add' },
+        { icon: walletOutline, label: 'Wallet' },
+        { icon: settingsOutline, label: 'Settings' },
+        // Add more buttons as needed
+      ],
+    };
+  },
+  methods: {
+
+  }
+});
+
 
 </script>
+
+<style scoped>
+.subtitle {
+  color: #948492;
+}
+
+ion-button{
+  max-width: 60px;
+  min-height: 60px;
+}
+
+.button-row {
+  display: flex;
+  overflow-x: auto;
+  padding: 8px;
+}
+
+.button-wrapper {
+  margin-right: 8px;
+}
+</style>
